d0c4d1c528ec3886cd2806ae3497a00963f47bc3
Peerapong Samarnpong <peerapong_sam@hotmail.com>
2020-06-18 11:02:29 +0700

    Add git hook script

---
+++ githooks/pre-push.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+./gradlew :app:assembleDevDebug
+
+status=$?
+
+if  [ "$status" = 0 ]; then
+    echo "Build success. Proceeding with push."
+    exit 0
+else
+    echo "Build failed. Please fix before pushing"
+    exit 1
+fi
