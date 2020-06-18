db2cc4fbb78401cd0f8afc1c50dc17aa3a4a58ed
Peerapong Samarnpong <peerapong_sam@hotmail.com>
2020-06-18 11:18:30 +0700

    Test build failed

--- app/src/main/java/com/peerapongsam/codelab/githook/MainActivity.kt
+++ app/src/main/java/com/peerapongsam/codelab/githook/MainActivity.kt
@@ -2,7 +2,6 @@ package com.peerapongsam.codelab.githook
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
